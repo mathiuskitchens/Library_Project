c08081c3d9dabb2dfca55d812c013e579a79c9d6
mathiuskitchens <mathiuskitchens@gmail.com>
2023-01-06 13:18:36 -0500

    Add initial JS file with tests

---
+++ app.js
@@ -0,0 +1,47 @@
+let myLibrary = [];
+
+function Book(title, author, pageCount, readStatus) {
+    // the constructor...
+      this.title = title
+      this.author = author
+      this.pageCount = pageCount
+      this.readStatus = readStatus
+      this.printInfo = function () { 
+          console.log(title + " by " + author + ", " + pageCount + " pages, " , readStatus)
+      }
+}
+
+
+
+
+
+function grabData() {
+    alert(data);
+    submit.preventdefault();
+};
+
+const data = document.getElementsByClassName("bookInfo");
+
+
+form.addEventListener("submit", grabData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addBookToLibrary() {
+
+
+    // do stuff here
+}
