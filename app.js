9c75a16e37b0c987a844c17431c61eced7fc22a0
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-01-14 20:36:13 -0500

    Wowza Linting...

--- app.js
+++ app.js
@@ -1,64 +1,38 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pageCount, readStatus) {
-    // the constructor...
-      this.title = title
-      this.author = author
-      this.pageCount = pageCount
-      this.readStatus = readStatus
-      this.printInfo = function () { 
-          console.log(title + " by " + author + ", " + pageCount + " pages, " , readStatus)
-      }
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+  this.printInfo = function () {
+    console.log(`${title} by ${author}, ${pageCount} pages, `, readStatus);
+  };
 }
-
-
-
-
-
-
 
 const data = document.getElementsByClassName("bookInfo");
 
+// const special = document.querySelectorAll('title');
+const test = document
+  .getElementById("myForm")
+  .getElementsByClassName("bookInfo");
 
-//const special = document.querySelectorAll('title');
-const test = document.getElementById('myForm').getElementsByClassName('bookInfo');
-
-
-
-
-//console.log(special);
-//console.log(test);
+// console.log(special);
+// console.log(test);
 console.log(test);
-
 
 document.querySelector("button").addEventListener("click", grabData);
 
+document.getElementById("submitButton").addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
 function grabData() {
-    console.log(test);
-   // submit.preventdefault();
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(test);
+  // submit.preventdefault();
+}
 
 function addBookToLibrary() {
-
-
-    // do stuff here
+  // do stuff here
 }
