51b9a67161311fd25711315fe79a433708e4d168
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-01-22 22:45:18 -0500

    fixed array input to constructor with spread oper

--- app.js
+++ app.js
@@ -6,24 +6,31 @@ function Book(title, author, pageCount, readStatus) {
   this.author = author;
   this.pageCount = pageCount;
   this.readStatus = readStatus;
-  this.printInfo = function () {
-    console.log(`${title} by ${author}, ${pageCount} pages, `, readStatus);
-  };
 }
 
 console.log("Waiting for a click...");
-
+// Get user input
 const submitButton = document.getElementById("submit");
 const form = document.getElementById("my-form");
 
-form?.addEventListener("submit", (event) => {
+form?.addEventListener("submit", (e) => {
   console.log("CLICK...");
-  event.preventDefault();
+  e.preventDefault();
   const formData = new FormData(form);
-
-  myLibrary.push(formData.getAll("form-info")?.valueOf());
-  console.log(myLibrary);
-
-  // console.log(formData.getAll("form-info")?.valueOf());
+  // Use constructor for newBook, assign form Data to it
+  const testBook = formData.getAll("form-info")?.valueOf();
+  const newBook = new Book(...testBook);
+  //  onst newBook = new Book(formData.getAll("form-info")?.valueOf());
+  // Test for success
+  console.log(newBook.title);
+  console.log(newBook.author);
+  console.log(newBook.pageCount);
+  return newBook;
+  // add book to page
+  // myLibrary.push(newBook);
+  // console.log(myLibrary);
 });
-// test
+
+function addNewBook(newBook) {}
+
+// Loop through array to display book on page
