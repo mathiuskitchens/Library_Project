0f133481a45f09aeaae8db4ef3e6470b2aaa331a
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-01-23 20:30:01 -0500

    Added ability to push book info to page

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ function Book(title, author, pageCount, readStatus) {
   this.pageCount = pageCount;
   this.readStatus = readStatus;
 }
-
 console.log("Waiting for a click...");
 // Get user input
 const submitButton = document.getElementById("submit");
 const form = document.getElementById("my-form");
+
+const bookParent = document.getElementById("book-parent");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+let printedReadStatus = "";
+// Function to add newBook to DOM
+
+input.focus();
 
 form?.addEventListener("submit", (e) => {
   console.log("CLICK...");
@@ -20,17 +27,45 @@ form?.addEventListener("submit", (e) => {
   // Use constructor for newBook, assign form Data to it
   const testBook = formData.getAll("form-info")?.valueOf();
   const newBook = new Book(...testBook);
-  //  onst newBook = new Book(formData.getAll("form-info")?.valueOf());
-  // Test for success
-  console.log(newBook.title);
-  console.log(newBook.author);
-  console.log(newBook.pageCount);
-  return newBook;
-  // add book to page
-  // myLibrary.push(newBook);
-  // console.log(myLibrary);
-});
 
-function addNewBook(newBook) {}
+  // Get read status for visual
+  console.log(newBook.readStatus);
+
+  // create placeholders for new book being added to DOM
+  const wholeBook = document.createElement("div");
+  const bookTitle = document.createElement("h2");
+  const bookAuthor = document.createElement("h3");
+  const bookPages = document.createElement("h3");
+  const readStatus = document.createElement("h3");
+
+  // Assign newBook object properties to placeholders
+  bookTitle.textContent = newBook.title;
+  bookAuthor.textContent = newBook.author;
+  bookPages.textContent = newBook.pageCount;
+
+  function updateStatus(readStatus) {
+    if (readStatus === "on") {
+      printedReadStatus = "Read";
+    } else {
+      printedReadStatus = "Not Read";
+    }
+  }
+  updateStatus();
+  readStatus.textContent = printedReadStatus;
+
+  // add to DOM
+  wholeBook.appendChild(bookTitle);
+  wholeBook.appendChild(bookAuthor);
+  wholeBook.appendChild(bookPages);
+  wholeBook.appendChild(readStatus);
+  bookParent.appendChild(wholeBook);
+
+  // add class to whole div to match styling
+  wholeBook.setAttribute("class", "book-card");
+
+  // Test for success
+  // refocus
+  input.focus();
+});
 
 // Loop through array to display book on page
